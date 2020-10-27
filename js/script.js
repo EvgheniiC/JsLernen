9b1b583db64138b890b6243d45e872cfa27e064c
Evghenii Costin <evghenii.costin@uni-oldenburg.de>
2020-10-27 11:34:35 +0100

    eng

--- js/script.js
+++ js/script.js
@@ -1,16 +1,23 @@
 /* Задания на урок:
 
 1) Удалить все рекламные блоки со страницы (правая часть сайта)
+Remove all ad units from the page (right side of the site)
 
 2) Изменить жанр фильма, поменять "комедия" на "драма"
+Change the genre of the film, change "comedy" to "drama"
 
 3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
 Реализовать только при помощи JS
+Change the background of the movie poster to "bg.jpg". It lies in the img folder.
+Implement with JS only
 
 4) Список фильмов на странице сформировать на основании данных из этого JS файла.
 Отсортировать их по алфавиту 
+Create a list of movies on the page based on the data from this JS file.
+Sort them alphabetically
 
-5) Добавить нумерацию выведенных фильмов */
+5) Добавить нумерацию выведенных фильмов 
+Add numbering of the output movies*/
 
 'use strict';
 
